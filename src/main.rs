1889fafa12e49395f5eaf812dbcb6d009a2925c1
h11 <nirisix@gmail.com>
2020-01-03 10:10:26 +0800

    rust version

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,72 @@
+use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
+use qrcodegen::{QrCode, QrCodeEcc};
+use qstring::QString;
 use structopt::StructOpt;
 
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
+#[structopt(name = "OTPAuth")]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    #[structopt(short, long)]
+    issuer: String,
+    #[structopt(short, long)]
+    account: String,
+    #[structopt(short, long)]
+    secret: String,
 }
 
 fn main() {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let otp = build_otp(&args.issuer, &args.account, &args.secret);
+    println!("{}", otp);
+    let qr = QrCode::encode_text(&otp, QrCodeEcc::Low).unwrap();
+    let text = to_terminal(&qr);
+    println!("{}", text);
+}
+
+fn build_otp(issuer: &str, account: &str, secret: &str) -> String {
+    // https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+    let parameters = QString::new(vec![
+        ("secret", secret.replace(" ", "")),
+        ("issuer", issuer.to_string()),
+    ]);
+    let issuer = utf8_percent_encode(issuer, NON_ALPHANUMERIC).to_string();
+    let otp = format!("otpauth://totp/{}:{}?{}", issuer, account, parameters);
+    otp
+}
+
+fn to_terminal(qr: &QrCode) -> String {
+    // https://superuser.com/a/1420015
+    let black = "\x1b[40m  \x1b[0m";
+    let white = "\x1b[47m  \x1b[0m";
+
+    let mut result = String::new();
+    for y in 0..qr.size() {
+        for x in 0..qr.size() {
+            if qr.get_module(x, y) {
+                result += black
+            } else {
+                result += white
+            }
         }
+        result += "\n"
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_otp() {
+        assert_eq!(
+            build_otp("Example", "alice@google.com", "JBSW Y3DP EHPK 3PXP"),
+            "otpauth://totp/Example:alice@google.com?secret=JBSWY3DPEHPK3PXP&issuer=Example"
+        );
+
+        assert_eq!(
+            build_otp("ACME Co", "john.doe@email.com","HXDM VJEC JJWS RB3H WIZR 4IFU GFTM XBOZ"),
+            "otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co"
+        );
     }
 }
